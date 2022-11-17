d3816b78f627a2d27a8cce2a96ca64b968a0dd0d
KirillDudarenko <103380080+KirillDudarenko@users.noreply.github.com>
2022-11-17 21:55:48 +0300

    Update Timer.tsx
    added 2 useEffects

--- classwork/src/components/Timer/Timer.tsx
+++ classwork/src/components/Timer/Timer.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Button } from './Button';
 import { Time } from './Time';
 const Timer = () => {
     const [seconds, setSeconds] = useState(0);
     const [timerId, setTimerId] = useState(0);
     const handleStartTimer = () => {
-        // setSeconds(0);
+        setSeconds(0);
         const id = setInterval(() => {
             setSeconds((prev) => prev + 1)
         }, 1000)
@@ -15,6 +15,20 @@ const Timer = () => {
         clearInterval(timerId);
         setTimerId(0);
     }
+    useEffect(() => {
+        console.log('Can start timer')
+    }, [])
+    useEffect(() => {
+        if (timerId !== 0) {
+            alert(timerId)
+        }
+    }, [timerId])
+    useEffect(() => {
+        if (seconds === 10){
+            handleStopTimer();
+            setSeconds(0);
+        }
+    }, [seconds])
     return (
         <div>
             <Time seconds={seconds}/>
